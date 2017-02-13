c12ab06b79a3fbe2694d322c59e1872109e4e38f
SadisticSun <tristan@iamt.nl>
2017-02-13 22:02:09 +0100

    Added styles and loading animations

--- api opdracht/static/js/app.js
+++ api opdracht/static/js/app.js
@@ -1,42 +1,55 @@
 /* Created by Tristan Jacobs on 13-02-17.
    Using AJA - Src: http://krampstudio.com/aja.js/
+   & Giphy API
 */
 
 (function(){
 "use strict";
 
 
-    var searchBox = document.getElementById('search'),
-        submit = document.getElementById('submit');
+    var _searchBox = document.getElementById('search'),
+        _submit = document.getElementById('submit'),
+        _loading = document.getElementById('loading');
 
-        submit.addEventListener("click", function () {
+        // clickhandler, executes main data retrieval function
+
+        _submit.addEventListener("click", function () {
             event.preventDefault();
             getSearchResults();
         });
         
         function getSearchResults () {
 
-            var userQuery = searchBox.value.split(' '),
-                urlForAPI = 'http://api.giphy.com/v1/gifs/search?q=' + userQuery + '&api_key=dc6zaTOxFJmzC';
+            var _urlForAPI = 'http://api.giphy.com/v1/gifs/search?q=' + _searchBox.value + '&api_key=dc6zaTOxFJmzC';
+
+            // Show loading icon
+            _loading.classList.remove('hidden');
 
             aja()
                 // url =
-                .url(urlForAPI)
+                .url(_urlForAPI)
+
+                //obj is a javascript object returned from Giphy API
                 .on('success', function(obj){
                     console.log(obj);
-                    //obj is a javascript object returned from Gyphy API
 
-                    var html;
+                    // remove loading icon
+                    _loading.classList.add('hidden');
+
+                    // display search results
+                    document.getElementById('search-result').innerHTML = 'Search results for ' + '"' + _searchBox.value + '"';
+
+                    var _html = '';
 
                     //map the objects received with AJA and for each object,
                     // set the <img src attribute to the 'embed_url' property of the object it loops over
 
                     obj.data.map(function(element){
-                        html += "<img src='https://i.giphy.com/" + element.id + ".gif" + "'>";
+                        _html += "<img src='https://i.giphy.com/" + element.id + ".gif" + "'>";
                     })
 
-                    // put the value of 'html' into the selected div
-                    document.getElementById('images').innerHTML = html;
+                    // put the value of '_html' into the selected div
+                    document.getElementById('images').innerHTML = _html;
 
                 })
                 .go();
