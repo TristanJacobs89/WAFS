b7d5ee2c6598d3270f72dbb255421904f2e107f7
SadisticSun <tristan@iamt.nl>
2017-02-16 19:07:38 +0100

    Changed stuff

--- opzet spa/static/js/app.js
+++ opzet spa/static/js/app.js
@@ -56,7 +56,7 @@
 
     function getSearchResults () {
 
-        var _urlForAPI = 'http://api.giphy.com/v1/gifs/search?q=' + _searchBox.value + '&api_key=dc6zaTOxFJmzC&limit=100';
+        var _urlForAPI = 'http://api.giphy.com/v1/gifs/search?q=' + _searchBox.value + '&api_key=dc6zaTOxFJmzC&limit=25';
 
         // Show loading icon
         _loading.classList.remove('hide');
@@ -68,25 +68,67 @@
             // obj is a javascript object returned from Giphy API
             .on('success', function(obj){
 
-                console.log(obj);
+                var _html = '';
+
                 // remove loading icon
                 _loading.classList.add('hide');
 
                 // display search results header
                 document.getElementById('search-result').innerHTML = 'Search results for ' + '"' + _searchBox.value + '"';
 
-                var _html = '';
+                /*
+                    map the objects received with AJA and for each object, set the <img src attribute
+                    to the 'embed_url' property of the object it loops over
+                */
 
-                // map the objects received with AJA and for each object,
-                // set the <img src attribute to the 'embed_url' property of the object it loops over
                 obj.data.map(function(element){
-                    _html += "<img src='https://i.giphy.com/" + element.id + ".gif" + "'>";
+                    _html += "<img class='gif' src='https://i.giphy.com/" + element.id + ".gif" + "'>";
                 });
 
-                // put the value of '_html' into the selected div to show them on the page
+
+                // put the values of '_html' into the selected div to show them on the page
                 document.getElementById('images').innerHTML = _html;
 
+                var _gifs = document.querySelectorAll('.gif');
+
+               // GIF clickhandler
+                _gifs.forEach(function(el){
+                    console.log(el);
+                    el.addEventListener('click', function () {
+
+                        // Get the modal
+                        var modal = document.getElementById('modal'),
+                            overlay = document.getElementById('overlay'),
+                            body = document.querySelector('body');
+
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks on the button, open the modal
+                        modal.style.display = 'block';
+                        overlay.classList.remove('hide');
+                        body.classList.add('hide-overflow')
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                            modal.style.display = 'none'
+                            overlay.classList.add('hide');
+                            body.classList.remove('hide-overflow')
+                        }
+
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = 'none';
+                                overlay.classList.add('hide');
+                                body.classList.remove('hide-overflow')
+                            }
+                        }
+                    });
+                });
             })
             .go();
     }
 })();
+
