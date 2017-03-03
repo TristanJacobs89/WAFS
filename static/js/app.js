c36fa73fab320a7ff55eeb93146d03d4c3a3ba4e
SadisticSun <tristan@iamt.nl>
2017-03-03 12:49:23 +0100

    Added comment

--- static/js/app.js
+++ static/js/app.js
@@ -83,7 +83,7 @@
     });
 
     function getSearchResults(filter) {
-
+        // Variable URL that gets sent to Giphy with Aja()
         var _urlForAPI = 'https://api.giphy.com/v1/' + filter + '/search?q=' + formElements.searchBox.value + '&api_key=dc6zaTOxFJmzC&limit=25';
 
         // Show loading icon
