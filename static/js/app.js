19f3fce17a3a99acf7c9cc960b0219f8024b84f0
SadisticSun <tristan@iamt.nl>
2017-03-24 22:44:59 +0100

    Comments

--- static/js/app.js
+++ static/js/app.js
@@ -13,10 +13,11 @@
 // TO DO:
 
 // Refactoring:
-// Alles in objecten met methods in plaats van floating functions.
-// Gebruik de patterns van het vak, zie Slack.
-// Nu te veel eigen structuur (is niet erg, maar nog niet volgbaar via de diagrammen)
-// Diagramm moet meer USER FLOW tonen; welke gebruikers interacties triggeren welke functionaliteiten?
+// - Alles in objecten met methods in plaats van floating functions.
+// - Gebruik de patterns van het vak, zie Slack.
+// - Nu te veel eigen structuur (is niet erg, maar nog niet volgbaar via de diagrammen)
+// - Diagram moet meer USER FLOW tonen; welke gebruikers interacties triggeren welke functionaliteiten?
+// - User Feedback: Wat gebeurt er als de data niet geladen kan worden? Ofs als de zoekterm niet herkend wordt?
 
 // =============================================
 
